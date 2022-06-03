985bea59ce75d9f4318b89f6c27d0366e86ab93f
Jan Heiderich <jan.heiderich@gmail.com>
2022-06-03 20:21:12 +0200

    init

---
+++ src/main/java/com/jan/coin/parqetparser/ParqetParserApplication.java
@@ -0,0 +1,13 @@
+package com.jan.coin.parqetparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqetParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqetParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jan/coin/parqetparser/ParqetParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan.coin.parqetparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqetParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
